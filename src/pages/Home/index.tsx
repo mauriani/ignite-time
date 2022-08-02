a51f46b08c8a6afcee8cd2e15f5c657559898a6a
mauriani <maurianim@gmail.com>
2022-08-02 16:53:51 -0300

    correção home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -18,14 +18,22 @@ export function Home() {
           <TaskInput
             type="text"
             id={"task"}
+            list={"task-suggestions"}
             placeholder="Dê um nome para o seu projeto"
           />
+          <datalist id="task-suggestions">
+            <option value="Projeto 1"></option>
+            <option value="banana"></option>
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
             id={"minutesAmount"}
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -21,6 +21,7 @@ export const FormContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+
   gap: 0.5rem;
   color: ${(props) => props.theme["gray-100"]};
   font-size: 1.125rem;
@@ -50,6 +51,10 @@ const BaseInput = styled.input`
 
 export const TaskInput = styled(BaseInput)`
   flex: 1;
+
+  &::-webkit-calendar-picker-indicator {
+    display: none !important;
+  }
 `;
 
 export const MinutesAmountInput = styled(BaseInput)`
